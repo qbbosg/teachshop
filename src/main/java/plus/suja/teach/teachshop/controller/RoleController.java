7f36a253f3336f74873dfaefb2f130b5b9f66565
qo <qbbo@outlook.com>
2024-04-06 19:08:06 +0800

    a role part
    * about a part role `create` api not all
    * extends CrudRepository remember set type such as `CrudRepository<Role, Integer>`

---
+++ src/main/java/plus/suja/teach/teachshop/controller/RoleController.java
@@ -0,0 +1,25 @@
+package plus.suja.teach.teachshop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import plus.suja.teach.teachshop.entity.Role;
+import plus.suja.teach.teachshop.service.RoleService;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/roles")
+public class RoleController {
+    @Autowired
+    private RoleService roleService;
+
+    @PostMapping
+    public String createRole(@RequestBody Map<String, Object> params) {
+        Role role = new Role();
+        role.setName((String)params.get("name"));
+        return roleService.create(role);
+    }
+}
---
+++ src/main/java/plus/suja/teach/teachshop/dao/RoleRepository.java
@@ -0,0 +1,7 @@
+package plus.suja.teach.teachshop.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import plus.suja.teach.teachshop.entity.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+}
