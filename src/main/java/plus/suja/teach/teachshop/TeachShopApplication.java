f62d6c5030081270ff92b4a04eacc280d4ba9a22
qo <qbbo@outlook.com>
2024-04-06 15:30:39 +0800

    init project

---
+++ src/main/java/plus/suja/teach/teachshop/TeachShopApplication.java
@@ -0,0 +1,13 @@
+package plus.suja.teach.teachshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/plus/suja/teach/teachshop/TeachShopApplicationTests.java
@@ -0,0 +1,13 @@
+package plus.suja.teach.teachshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
