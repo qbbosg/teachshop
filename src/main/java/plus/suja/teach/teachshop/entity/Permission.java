74cef73673f36f73d766c13309c2430d489cf6d2
qo <qbbo@outlook.com>
2024-04-06 19:32:15 +0800

    a permission part like role ago
    * set role permission, one role have many permissions

---
+++ src/main/java/plus/suja/teach/teachshop/entity/Permission.java
@@ -0,0 +1,30 @@
+package plus.suja.teach.teachshop.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "permission")
+public class Permission extends Base {
+    @Column(name = "role_id")
+    private Integer roleId;
+    @Column(name = "name")
+    private String name;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
