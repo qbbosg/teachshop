b4cc79874480fe07b1a95aaa5346af874dffc7de
qbbosg <qbbo@outlook.sg>
2024-04-07 10:35:41 +0800

    Error: runtime error

--- src/main/java/plus/suja/teach/teachshop/entity/Permission.java
+++ src/main/java/plus/suja/teach/teachshop/entity/Permission.java
@@ -2,6 +2,7 @@ package plus.suja.teach.teachshop.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,6 +12,16 @@ public class Permission extends Base {
     private Integer roleId;
     @Column(name = "name")
     private String name;
+    @ManyToOne
+    private Role role;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
     public Integer getRoleId() {
         return roleId;
--- src/main/java/plus/suja/teach/teachshop/entity/Role.java
+++ src/main/java/plus/suja/teach/teachshop/entity/Role.java
@@ -2,13 +2,31 @@ package plus.suja.teach.teachshop.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "role")
 public class Role extends Base {
     @Column(name = "name")
     private String name;
+    @OneToMany
+    @JoinTable(name = "permission",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "id"))
+    private Set<Permission> permissions;
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
 
     public String getName() {
         return name;
