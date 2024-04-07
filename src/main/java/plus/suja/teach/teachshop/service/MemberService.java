b012792ec2cf457ae43e529bd61c0f655eb0ab1f
qbbosg <qbbo@outlook.sg>
2024-04-07 09:51:42 +0800

    Feature member find all roles.
    * The member with role relation is many to many.

--- src/main/java/plus/suja/teach/teachshop/service/MemberService.java
+++ src/main/java/plus/suja/teach/teachshop/service/MemberService.java
@@ -2,20 +2,34 @@ package plus.suja.teach.teachshop.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import plus.suja.teach.teachshop.dao.UserRepository;
+import plus.suja.teach.teachshop.dao.MemberRepository;
 import plus.suja.teach.teachshop.entity.Member;
+import plus.suja.teach.teachshop.entity.Role;
+
+import java.util.stream.Collectors;
 
 @Service
 public class MemberService {
     @Autowired
-    private UserRepository userRepository;
+    private MemberRepository memberRepository;
 
     public String login() {
         return "login";
     }
 
     public String register(Member member) {
-        userRepository.save(member);
+        memberRepository.save(member);
         return "register";
+    }
+
+    public String all() {
+        memberRepository.findAll().forEach(member -> {
+            System.out.print("用户：");
+            System.out.println(member.getUsername());
+            System.out.print("身份：");
+            System.out.println(member.getRoles().stream().map(Role::getName).collect(Collectors.toList()));
+            System.out.println("--------------");
+        });
+        return "all";
     }
 }
