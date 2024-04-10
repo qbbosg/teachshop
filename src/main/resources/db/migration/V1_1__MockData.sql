b3fa188d4d805ed9b7555cb6651337b355e6c1f4
qbbosg <qbbo@outlook.sg>
2024-04-10 22:11:40 +0800

    Formatter code

--- src/main/resources/db/migration/V1_1__MockData.sql
+++ src/main/resources/db/migration/V1_1__MockData.sql
@@ -1,30 +1,56 @@
-insert into member (username, encrypt_password) values ( 'li', 'pwd1' );
-insert into member (username, encrypt_password) values ( 'wu', 'pwd1' );
-insert into member (username, encrypt_password) values ( 'liu', 'pwd1' );
-insert into member (username, encrypt_password) values ( 'xu', 'pwd1' );
-insert into member (username, encrypt_password) values ( 'lei', 'pwd1' );
+insert into member (username, encrypt_password)
+values ('li', 'pwd1');
+insert into member (username, encrypt_password)
+values ('wu', 'pwd1');
+insert into member (username, encrypt_password)
+values ('liu', 'pwd1');
+insert into member (username, encrypt_password)
+values ('xu', 'pwd1');
+insert into member (username, encrypt_password)
+values ('lei', 'pwd1');
 
-insert into role (name) values ( 'student' );
-insert into role (name) values ( 'teacher' );
-insert into role (name) values ( 'admin' );
-insert into role (name) values ( 'root' );
+insert into role (name)
+values ('student');
+insert into role (name)
+values ('teacher');
+insert into role (name)
+values ('admin');
+insert into role (name)
+values ('root');
 
-insert into permission (name, role_id) values ( 'login', 1 );
-insert into permission (name, role_id) values ( 'login', 2 );
-insert into permission (name, role_id) values ( 'upload', 2 );
-insert into permission (name, role_id) values ( 'mod_user', 3 );
-insert into permission (name, role_id) values ( 'upload', 3 );
-insert into permission (name, role_id) values ( 'mod_user', 4 );
-insert into permission (name, role_id) values ( 'upload', 4 );
-insert into permission (name, role_id) values ( 'mod_admin', 4 );
+insert into permission (name, role_id)
+values ('login', 1);
+insert into permission (name, role_id)
+values ('login', 2);
+insert into permission (name, role_id)
+values ('upload', 2);
+insert into permission (name, role_id)
+values ('mod_user', 3);
+insert into permission (name, role_id)
+values ('upload', 3);
+insert into permission (name, role_id)
+values ('mod_user', 4);
+insert into permission (name, role_id)
+values ('upload', 4);
+insert into permission (name, role_id)
+values ('mod_admin', 4);
 
-insert into member_role (member_id, role_id) values ( 1, 0 );
-insert into member_role (member_id, role_id) values ( 2, 1 );
-insert into member_role (member_id, role_id) values ( 3, 2 );
-insert into member_role (member_id, role_id) values ( 4, 3 );
-insert into member_role (member_id, role_id) values ( 5, 4 );
+insert into member_role (member_id, role_id)
+values (1, 1);
+insert into member_role (member_id, role_id)
+values (2, 1);
+insert into member_role (member_id, role_id)
+values (3, 2);
+insert into member_role (member_id, role_id)
+values (4, 3);
+insert into member_role (member_id, role_id)
+values (5, 4);
 
-insert into session (member_id, cookie) values ( 1,  'student1');
-insert into session (member_id, cookie) values ( 3,  'teacher');
-insert into session (member_id, cookie) values ( 4,  'admin');
-insert into session (member_id, cookie) values ( 5,  'root');
+insert into session (member_id, cookie)
+values (1, 'student1');
+insert into session (member_id, cookie)
+values (3, 'teacher');
+insert into session (member_id, cookie)
+values (4, 'admin');
+insert into session (member_id, cookie)
+values (5, 'root');
